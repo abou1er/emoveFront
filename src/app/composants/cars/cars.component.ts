66f17f2d2d7082616bd8ad51f31a9d8561d69eec
elomag <elodie.marco13127@gmail.com>
2022-06-29 12:06:56 +0200

    ngFor cars.component OK - Elo

--- src/app/composants/cars/cars.component.ts
+++ src/app/composants/cars/cars.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { ProductsService } from 'src/app/services/products.service';
 import { UsersService } from 'src/app/services/users.service';
 
-
 @Component({
   selector: 'app-cars',
   templateUrl: './cars.component.html',
@@ -11,9 +10,21 @@ import { UsersService } from 'src/app/services/users.service';
 })
 export class CarsComponent implements OnInit {
 
+Cars: any;
+
+
   constructor(private productsservice: ProductsService, private usersservice: UsersService) { }
 
   ngOnInit(): void {
+  this.getCars()
+  }
+
+  getCars(){
+    this.productsservice.getVoitures().subscribe(data =>{
+      this.Cars = data
+      console.log(this.Cars);
+      
+    })
   }
 
 }
